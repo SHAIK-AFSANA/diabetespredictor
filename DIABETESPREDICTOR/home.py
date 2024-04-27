8d0e4feb5cc991e9b69aff1cc71ceb27abe7af13
SHAIK AFSANA <146961917+SHAIK-AFSANA@users.noreply.github.com>
2024-04-27 16:08:25 -0500

    Update home.py

--- DIABETESPREDICTOR/home.py
+++ DIABETESPREDICTOR/home.py
@@ -10,7 +10,7 @@ def app():
     understanding the risk factors is crucial for early detection and management of diabetes.
     """)
     
-    st.image("Images/Diabetes-type-2.jpg",use_column_width=True)
+    st.image("DIABETESPREDICTOR/Images/Diabetes-type-2.jpg-Diabetes.jpg",use_column_width=True)
     # Information about the "3 P's" of diabetes
     
     st.header("Symptoms of Diabetes")
@@ -23,7 +23,7 @@ def app():
     # Information about Polyuria
     with col1:
         st.subheader("Polyuria")
-        st.image("Images/polyuria.jpg", use_column_width=True)
+        st.image("DIABETESPREDICTOR/Images/polyuria.jpg", use_column_width=True)
         st.write("""
         Polyuria refers to excessive urination. It is one of the classic symptoms of diabetes and occurs when the kidneys 
         filter too much glucose from the blood, leading to increased urine production. People with diabetes may experience 
@@ -33,7 +33,7 @@ def app():
     # Information about Polydipsia
     with col2:
         st.subheader("Polydipsia")
-        st.image("Images/polydipsia.webp", use_column_width=True)
+        st.image("DIABETESPREDICTOR/Images/polydipsia.webp", use_column_width=True)
         st.write("""
         Polydipsia is excessive thirst. It is closely related to polyuria because the body tries to compensate for the fluid 
         lost through excessive urination by increasing the sensation of thirst. People with diabetes may have an unquenchable 
@@ -43,7 +43,7 @@ def app():
     # Information about Polyphagia
     with col3:
         st.subheader("Polyphagia")
-        st.image("Images/polyphagia.webp", use_column_width=True)
+        st.image("DIABETESPREDICTOR/Images/polyphagia.webp", use_column_width=True)
         st.write("""
         Polyphagia is excessive hunger or increased appetite. Despite eating regularly, people with diabetes may still feel 
         hungry because their cells are unable to absorb glucose properly, leading to a constant feeling of hunger. This can 
@@ -56,7 +56,7 @@ def app():
     col4, col5, col6=st.columns(3)
     with col4:
         with st.expander("SUDDEN WEIGHT LOSS "):
-            st.image("Images/suddenweightloss.webp", use_column_width=True)
+            st.image("DIABETESPREDICTOR/Images/suddenweightloss.webp", use_column_width=True)
             st.write("""
             Losing weight without trying, despite maintaining regular eating habits, often due to the body's inability to properly utilize glucose for energy, resulting in the breakdown of muscle and fat tissues for fuel.
             """)
@@ -64,7 +64,7 @@ def app():
     # Information about Polydipsia
     with col5:
         with st.expander("WEAKNESS "):
-            st.image("Images/weakness.webp", use_column_width=True)
+            st.image("DIABETESPREDICTOR/Images/weakness.webp", use_column_width=True)
             st.write("""
             Feeling unusually tired or weak, which may be caused by fluctuations in blood sugar levels and the body's inability to effectively transport glucose to cells for energy.
             """)
@@ -72,7 +72,7 @@ def app():
     # Information about Polyphagia
     with col6:
         with st.expander("GENITAL THRUSH "):
-            st.image("Images/genital thrush.webp", use_column_width=True)
+            st.image("DIABETESPREDICTOR/Images/genital thrush.webp", use_column_width=True)
             st.write("""
             Developing recurrent yeast infections, particularly in the genital area, due to the excess sugar in the urine providing an ideal environment for yeast to grow.
             """)
@@ -80,7 +80,7 @@ def app():
     col7, col8, col9=st.columns(3)
     with col7:
         with st.expander("VISUAL BLURRING "):
-            st.image("Images/sight.webp", use_column_width=True)
+            st.image("DIABETESPREDICTOR/Images/sight.webp", use_column_width=True)
             st.write("""
              Experiencing blurred or distorted vision as high levels of blood glucose can affect the shape and function of the lens in the eye, leading to changes in vision.
             """)
@@ -88,7 +88,7 @@ def app():
     # Information about Polydipsia
     with col8:
         with st.expander("ITCHING "):
-            st.image("Images/itching.webp", use_column_width=True)
+            st.image("DIABETESPREDICTOR/Images/itching.webp", use_column_width=True)
             st.write("""
             Experiencing itching or irritation in the genital area, often due to yeast infections caused by the overgrowth of Candida fungus fueled by high blood sugar levels.
             """)
@@ -96,7 +96,7 @@ def app():
     # Information about Polyphagia
     with col9:
         with st.expander("IRRITABILITY "):
-            st.image("Images/irritability.webp", use_column_width=True)
+            st.image("DIABETESPREDICTOR/Images/irritability.webp", use_column_width=True)
             st.write("""
             Experiencing mood swings, irritability, or difficulty concentrating, which may be attributed to fluctuations in blood sugar levels affecting brain function and mood regulation.
             """)
@@ -104,7 +104,7 @@ def app():
     col10, col11, col12=st.columns(3)
     with col10:
         with st.expander("DELAYED HEALING "):
-            st.image("Images/delayed healing.webp", use_column_width=True)
+            st.image("DIABETESPREDICTOR/Images/delayed healing.webp", use_column_width=True)
             st.write("""
              Noticing wounds or sores taking longer to heal than usual, as high blood sugar levels can impair the body's ability to repair damaged tissue and fight infections.
             """)
@@ -112,7 +112,7 @@ def app():
     # Information about Polydipsia
     with col11:
         with st.expander("PARTIAL PARESIS  "):
-            st.image("Images/partialparesis.webp", use_column_width=True)
+            st.image("DIABETESPREDICTOR/Images/partialparesis.webp", use_column_width=True)
             st.write("""
              Experiencing numbness, tingling, or pain, particularly in the hands and feet, due to nerve damage (neuropathy) caused by prolonged exposure to high levels of glucose in the bloodstream.
             """)
@@ -120,7 +120,7 @@ def app():
     # Information about Polyphagia
     with col12:
         with st.expander("MUSCLE STIFFNESS "):
-            st.image("Images/musclestiffness.webp", use_column_width=True)
+            st.image("DIABETESPREDICTOR/Images/musclestiffness.webp", use_column_width=True)
             st.write("""
             Feeling stiffness or cramping in the muscles, which may be caused by dehydration or electrolyte imbalances resulting from frequent urination and loss of fluids.
             """)
@@ -128,7 +128,7 @@ def app():
     col13, col14=st.columns(2)
     with col13:
         with st.expander("ALOPECIA (HAIR LOSS)  "):
-            st.image("Images/hairloss.webp", use_column_width=True)
+            st.image("DIABETESPREDICTOR/Images/hairloss.webp", use_column_width=True)
             st.write("""
               Experiencing hair thinning or loss, particularly on the scalp or other parts of the body, which may be attributed to poor circulation or changes in hormone levels associated with diabetes.
             """)
@@ -136,7 +136,7 @@ def app():
     # Information about Polydipsia
     with col14:
         with st.expander("OBESITY "):
-            st.image("Images/obesity.webp", use_column_width=True)
+            st.image("DIABETESPREDICTOR/Images/obesity.webp", use_column_width=True)
             st.write("""
              Being overweight or obese, particularly with excess abdominal fat, increases the risk of developing type 2 diabetes by causing insulin resistance and impairing glucose metabolism.
             """)
